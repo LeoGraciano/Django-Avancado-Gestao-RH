edb2999d9d8bfc0debb75893f78d13a427f948b3
LeoGraciano <lgraciano@blockitsec.com>
2021-04-03 18:43:48 -0300

    novo ip

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'sy3z7=qhunl^h^wz63q@&%yv*@#9kauwrupr#ynzxv^5xbr)4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['54.207.167.9']
 
 
 # Application definition
