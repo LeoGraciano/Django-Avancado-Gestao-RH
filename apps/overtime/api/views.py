86dafec9e927d05515e6b2509ce99324a955f85b
LeoGraciano <lgraciano@blockitsec.com>
2021-04-12 07:00:50 -0300

    authe api teste deploy

--- apps/overtime/api/views.py
+++ apps/overtime/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 from apps.overtime.models import OverTime
 from .serializers import OverTimeSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 # API
 class OverTimeViewSet(viewsets.ModelViewSet):
     queryset = OverTime.objects.all()
     serializer_class = OverTimeSerializer
+    # authentication e permissions Manual
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
--- plataforma/settings.py
+++ plataforma/settings.py
@@ -93,16 +93,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-# authentication e permissions Geral
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated'
+    # ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ]
 }
 
 # Password validation
